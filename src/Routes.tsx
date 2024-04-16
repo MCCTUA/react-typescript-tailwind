5bae8d08a3c7a5e6d9c07eea84e5a209b8d9a556
MCCTUA <mcctua@gmail.com>
2024-04-16 10:38:06 +0700

    page about, team, project with route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Frontlayout from './components/layouts/Frontlayout';
 import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Team from './pages/team/Team';
+import Project from './pages/project/Project';
 
 const AppRoutes = () => {
   return (
@@ -11,6 +14,30 @@ const AppRoutes = () => {
         element={
           <Frontlayout>
             <Home />
+          </Frontlayout>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <Frontlayout>
+            <About />
+          </Frontlayout>
+        }
+      />
+      <Route
+        path="/teams"
+        element={
+          <Frontlayout>
+            <Team />
+          </Frontlayout>
+        }
+      />
+      <Route
+        path="/projects"
+        element={
+          <Frontlayout>
+            <Project />
           </Frontlayout>
         }
       />
